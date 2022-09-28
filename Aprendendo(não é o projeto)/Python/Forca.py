c9f96c63e64143524ce85f933457f20b0a01d195
Josue <josuebvrios@gmail.com>
2022-09-28 16:04:47 -0300

    Novo projeto: Forca

---
+++ Aprendendo(não é o projeto)/Python/Forca.py
@@ -0,0 +1,10 @@
+
+
+# Isso aqui é só pra deixar o output mais bonito
+print("--------------------------------") #
+print("---Bem vindo ao jogo de Forca---") # Imprime na tela
+print("--------------------------------") #
+
+# Isso aqui é indicar que acabou o jogo
+print("Fim do jogo") # Imprime na tela
+

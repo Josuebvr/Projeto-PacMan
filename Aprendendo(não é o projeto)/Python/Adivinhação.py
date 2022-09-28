97fbda2b312021f8df0a655d26ff59da03be3914
Josue <josuebvrios@gmail.com>
2022-09-28 12:31:37 -0300

    Implementação de comentários

--- Aprendendo(não é o projeto)/Python/Adivinhação.py
+++ Aprendendo(não é o projeto)/Python/Adivinhação.py
@@ -1,33 +1,36 @@
-print("--------------------------------")
-print("Bem vindo ao jogo de adivinhação")
-print("--------------------------------")
-
-numero_secreto = 42
-total_de_tentativas = 3
+import random # Importa a biblioteca random
 
 
+print("--------------------------------") #
+print("Bem vindo ao jogo de adivinhação") # Imprime na tela
+print("--------------------------------") #
 
-for rodada in range(1, total_de_tentativas + 1):
-    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute = input("Digite um número entre 1 e 100: ")
-    print("Você digitou ", chute)
-    chute = int(chute)
+numero_secreto = round(random.random() * 100) #random.random() gera um número aleatório entre 0 e 1
+total_de_tentativas = 3 # Define o número de tentativas
+
+
+
+for rodada in range(1, total_de_tentativas + 1): #range(1, 4) = 1, 2, 3
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas)) # Imprime na tela
+    chute = input("Digite um número entre 1 e 100: ") # Imprime na tela e recebe um valor
+    print("Você digitou ", chute) # Imprime na tela
+    chute = int(chute) # Converte o valor recebido para inteiro
     
-    if(chute < 1 or chute > 100):
-        print("Você deve digitar um número entre 1 e 100!")
-        continue
+    if(chute < 1 or chute > 100): # Verifica se o valor está entre 1 e 100
+        print("Você deve digitar um número entre 1 e 100!") # Imprime na tela
+        continue # Volta para o início do loop
 
-    acertou = chute == numero_secreto
-    maior = chute > numero_secreto
-    menor = chute < numero_secreto
+    acertou = chute == numero_secreto # Verifica se o valor é igual ao número secreto
+    maior = chute > numero_secreto # Verifica se o valor é maior que o número secreto
+    menor = chute < numero_secreto # Verifica se o valor é menor que o número secreto
 
-    if (acertou):
-        print("Você acertou!")
-        break
-    else:
-        if (maior):
-            print("Você errou! O seu chute foi maior que o número secreto.")
-        elif (menor):
-            print("Você errou! O seu chute foi menor que o número secreto.")
+    if (acertou): # Verifica se o valor é igual ao número secreto
+        print("Você acertou!") # Imprime na tela
+        break # Sai do loop
+    else: # Se o valor não for igual ao número secreto
+        if (maior): # Verifica se o valor é maior que o número secreto
+            print("Você errou! O seu chute foi maior que o número secreto.") # Imprime na tela
+        elif (menor): # Verifica se o valor é menor que o número secreto
+            print("Você errou! O seu chute foi menor que o número secreto.") # Imprime na tela
     
-print("Fim do jogo")
+print("Fim do jogo") # Imprime na tela

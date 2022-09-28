9c0bd64b71f39cb5e6dc297504c437d36fcb7a6a
Josue <josuebvrios@gmail.com>
2022-09-28 16:12:38 -0300

    Implementação da escolha de jogos

---
+++ Aprendendo(não é o projeto)/Python/Jogos.py
@@ -0,0 +1,19 @@
+import Forca # Importa o arquivo Forca.py
+import Adivinhação # Importa o arquivo Adivinhação.py
+
+# Isso aqui é só pra deixar o output mais bonito
+print("--------------------------------") #
+print("-------Escolha o seu jogo-------") # Imprime na tela
+print("--------------------------------") #
+
+# Isso aqui é pra definir o jogo
+print("(1) Forca (2) Adivinhação") # Imprime na tela
+
+jogo = int(input("Qual jogo? ")) # Pede pra definir o jogo
+
+
+if (jogo == 1): # Se o jogo for 1
+    print("Jogando Forca") 
+elif (jogo == 2): # Se o jogo for 2
+    print("Jogando Adivinhação") 
+

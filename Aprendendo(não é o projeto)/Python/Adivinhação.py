8671237c293d9822b6aae7a0cc077ea7109d83af
Josue <josuebvrios@gmail.com>
2022-09-28 10:56:24 -0300

    Organização e edição do py

--- Aprendendo(não é o projeto)/Python/Adivinhação.py
+++ Aprendendo(não é o projeto)/Python/Adivinhação.py
@@ -1,13 +1,13 @@
-print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+print("--------------------------------")
 print("Bem vindo ao jogo de adivinhação")
-print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+print("--------------------------------")
 
 numero_secreto = 42
 total_de_tentativas = 3
-rodada = 1
 
 
-while (rodada <= total_de_tentativas ):
+
+for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
     chute = input("Digite o seu número: ")
     print("Você digitou ", chute)
@@ -24,7 +24,5 @@ while (rodada <= total_de_tentativas ):
             print("Você errou! O seu chute foi maior que o número secreto.")
         elif (menor):
             print("Você errou! O seu chute foi menor que o número secreto.")
-            
-    rodada = rodada + 1
     
 print("Fim do jogo")

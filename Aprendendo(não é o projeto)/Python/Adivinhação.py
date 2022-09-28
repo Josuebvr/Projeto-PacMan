525f8048dc3cf8fdd7dada459521a11fe9ae64ad
Josue <josuebvrios@gmail.com>
2022-09-28 12:35:40 -0300

    Alteração e inclusão de comentários

--- Aprendendo(não é o projeto)/Python/Adivinhação.py
+++ Aprendendo(não é o projeto)/Python/Adivinhação.py
@@ -1,21 +1,23 @@
 import random # Importa a biblioteca random
 
-
+# Isso aqui é só pra deixar o output mais bonito
 print("--------------------------------") #
 print("Bem vindo ao jogo de adivinhação") # Imprime na tela
 print("--------------------------------") #
 
-numero_secreto = round(random.random() * 100) #random.random() gera um número aleatório entre 0 e 1
+# Isso aqui é pra definir o número secreto
+numero_secreto = round(random.random() * 100) # random.random() gera um número aleatório entre 0 e 1
 total_de_tentativas = 3 # Define o número de tentativas
 
 
-
-for rodada in range(1, total_de_tentativas + 1): #range(1, 4) = 1, 2, 3
+# Isso aqui é pra definir o nível de dificuldade
+for rodada in range(1, total_de_tentativas + 1): # range(1, 4) = 1, 2, 3
     print("Tentativa {} de {}".format(rodada, total_de_tentativas)) # Imprime na tela
     chute = input("Digite um número entre 1 e 100: ") # Imprime na tela e recebe um valor
     print("Você digitou ", chute) # Imprime na tela
     chute = int(chute) # Converte o valor recebido para inteiro
     
+    # Isso aqui é pra verificar se o chute está correto
     if(chute < 1 or chute > 100): # Verifica se o valor está entre 1 e 100
         print("Você deve digitar um número entre 1 e 100!") # Imprime na tela
         continue # Volta para o início do loop
@@ -24,6 +26,7 @@ for rodada in range(1, total_de_tentativas + 1): #range(1, 4) = 1, 2, 3
     maior = chute > numero_secreto # Verifica se o valor é maior que o número secreto
     menor = chute < numero_secreto # Verifica se o valor é menor que o número secreto
 
+# Isso aqui é pra imprimir o resultado
     if (acertou): # Verifica se o valor é igual ao número secreto
         print("Você acertou!") # Imprime na tela
         break # Sai do loop
@@ -33,4 +36,5 @@ for rodada in range(1, total_de_tentativas + 1): #range(1, 4) = 1, 2, 3
         elif (menor): # Verifica se o valor é menor que o número secreto
             print("Você errou! O seu chute foi menor que o número secreto.") # Imprime na tela
     
+# Isso aqui é indicar que acabou o jogo
 print("Fim do jogo") # Imprime na tela

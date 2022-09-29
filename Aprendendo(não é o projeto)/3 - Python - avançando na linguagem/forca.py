cd4b27bd6569f11e40f3096492455dc4e751b4b2
Josué <josuebvrios@gmail.com>
2022-09-29 18:30:11 -0300

    Game Loop

--- Aprendendo(não é o projeto)/3 - Python - avançando na linguagem/forca.py
+++ Aprendendo(não é o projeto)/3 - Python - avançando na linguagem/forca.py
@@ -4,13 +4,17 @@ def jogar(): # Função jogo_forca
     print("--------------------------------") #
     print("---Bem vindo ao jogo de Forca---") # Imprime na tela
     print("--------------------------------") #
+    
+    palavra_secreta = "banana" # Palavra secreta
+    
+    enforcou = False # Variável que diz se o jogador foi enforcado
+    acertou = False # Variável que diz se o jogador acertou a palavra secreta
+    
+    while(not enforcou and not acertou): # Enquanto o jogador não for enforcado e não acertar a palavra secreta
+        print("Jogando...") # Imprime que ainda está jogando	
 
     # Isso aqui é indicar que acabou o jogo
     print("Fim do jogo") # Imprime na tela
-
-
-
-
 
 if(__name__ == "__main__"): # Se o arquivo for executado diretamente
     jogar() # Executa a função de jogar

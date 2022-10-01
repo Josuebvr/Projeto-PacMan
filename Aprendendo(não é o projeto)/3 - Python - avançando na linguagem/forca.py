42278af6a24a66145912201b8739bd9b940c642f
Josué <josuebvrios@gmail.com>
2022-10-01 10:28:14 -0300

    Lista de letras acertadas

--- Aprendendo(não é o projeto)/3 - Python - avançando na linguagem/forca.py
+++ Aprendendo(não é o projeto)/3 - Python - avançando na linguagem/forca.py
@@ -6,24 +6,30 @@ def jogar():  # Função jogo_forca
     print("--------------------------------")
 
     palavra_secreta = "banana"  # Palavra secreta
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]  # Lista de letras acertadas
 
     enforcou = False  # Variável que diz se o jogador foi enforcado
     acertou = False  # Variável que diz se o jogador acertou a palavra secreta
+    
+    print(letras_acertadas)  # Imprime na tela a prévia de quantas letras tem a palavra secreta
 
     # Enquanto o jogador não for enforcado e não acertar a palavra secreta
     while (not enforcou and not acertou):
 
         # Pede para o jogador digitar uma letra
-        chute = input("Qual a letra? ") # Pede para o jogador digitar uma letra
+        # Pede para o jogador digitar uma letra
+        chute = input("Qual a letra? ")
         chute = chute.strip()  # Remove os espaços em branco
 
         index = 0  # Variável que vai guardar o índice da letra
         for letra in palavra_secreta:  # Para cada letra na palavra secreta
-            if (chute.upper() == letra.upper()):  # Se o chute for igual a letra (ignorando maiúsculas e minúsculas)
-                print("Encontrei a letra {} na posição {}".format(letra, index))  # Imprime na tela caso o chute seja igual a letra
+            # Se o chute for igual a letra (ignorando maiúsculas e minúsculas)
+            if (chute.upper() == letra.upper()):
+                # Imprime na tela caso o chute seja igual a letra
+                letras_acertadas[index] = letra # Coloca a letra na lista de letras acertadas
             index = index + 1  # Incrementa o índice
 
-        print("Jogando...")  # Imprime que ainda está jogando
+        print(letras_acertadas)  # Imprime na tela a lista de letras acertadas
 
     # Isso aqui é indicar que acabou o jogo
     print("Fim do jogo")  # Imprime na tela

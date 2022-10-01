e622b21a71bad3aa1f7926dda64c90ee53000993
Josué <josuebvrios@gmail.com>
2022-10-01 10:55:13 -0300

    Implementação de tentativas de erros

--- Aprendendo(não é o projeto)/3 - Python - avançando na linguagem/forca.py
+++ Aprendendo(não é o projeto)/3 - Python - avançando na linguagem/forca.py
@@ -5,11 +5,12 @@ def jogar():  # Função jogo_forca
     print("---Bem vindo ao jogo de Forca---")  # Imprime na tela
     print("--------------------------------")
 
-    palavra_secreta = "banana"  # Palavra secreta
+    palavra_secreta = "banana".upper()  # Palavra secreta
     letras_acertadas = ["_", "_", "_", "_", "_", "_"]  # Lista de letras acertadas
 
     enforcou = False  # Variável que diz se o jogador foi enforcado
     acertou = False  # Variável que diz se o jogador acertou a palavra secreta
+    erros = 0  # Variável que conta os erros
     
     print(letras_acertadas)  # Imprime na tela a prévia de quantas letras tem a palavra secreta
 
@@ -19,21 +20,28 @@ def jogar():  # Função jogo_forca
         # Pede para o jogador digitar uma letra
         # Pede para o jogador digitar uma letra
         chute = input("Qual a letra? ")
-        chute = chute.strip()  # Remove os espaços em branco
-
-        index = 0  # Variável que vai guardar o índice da letra
-        for letra in palavra_secreta:  # Para cada letra na palavra secreta
-            # Se o chute for igual a letra (ignorando maiúsculas e minúsculas)
-            if (chute.upper() == letra.upper()):
-                # Imprime na tela caso o chute seja igual a letra
-                letras_acertadas[index] = letra # Coloca a letra na lista de letras acertadas
-            index = index + 1  # Incrementa o índice
-
+        chute = chute.strip().upper()  # Remove os espaços em branco e deixa tudo em maiúsculo
+        
+        if(chute in palavra_secreta): # Se a letra digitada estiver na palavra secreta
+            index = 0  # Variável que vai guardar o índice da letra
+            for letra in palavra_secreta:  # Para cada letra na palavra secreta
+                # Se o chute for igual a letra
+                if (chute == letra): # Imprime na tela caso o chute seja igual a letra
+                    letras_acertadas[index] = letra # Coloca a letra na lista de letras acertadas
+                index += 1  # Incrementa o índice
+        else:
+            erros += 1  # Incrementa o número de erros
+            
+        enforcou = erros == 6  # Se o número de erros for igual a 6, o jogador é enforcado
+        acertou = "_" not in letras_acertadas  # Se não tiver mais "_" na lista de letras acertadas, o jogador acertou a palavra secreta
         print(letras_acertadas)  # Imprime na tela a lista de letras acertadas
 
     # Isso aqui é indicar que acabou o jogo
-    print("Fim do jogo")  # Imprime na tela
-
+    if(acertou):
+        print("Você ganhou!") 
+    else:
+        print("Você perdeu!") 
+    print("Fim do jogo")  
 
 if (__name__ == "__main__"):  # Se o arquivo for executado diretamente
     jogar()  # Executa a função de jogar

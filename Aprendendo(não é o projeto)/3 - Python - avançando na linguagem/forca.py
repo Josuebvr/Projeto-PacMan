66214709113ad059b3002dd0a3c95f2828832659
Josué <josuebvrios@gmail.com>
2022-10-03 09:58:39 -0300

    Organizando o código em funções

--- Aprendendo(não é o projeto)/3 - Python - avançando na linguagem/forca.py
+++ Aprendendo(não é o projeto)/3 - Python - avançando na linguagem/forca.py
@@ -1,27 +1,10 @@
+import random  # Importa a biblioteca random
+
 def jogar():  # Função jogo_forca
 
-    # Isso aqui é só pra deixar o output mais bonito
-    print("--------------------------------")
-    print("---Bem vindo ao jogo de Forca---")  # Imprime na tela
-    print("--------------------------------")
-    
-    # Nada daqui funciona
-     #               arquivo = open("palavras.txt", "r")  # Abre o arquivo
-      #              palavras = []  # Cria uma lista vazia
-       #             
-        #            for linha in arquivo:  # Para cada linha no arquivo
-         #               linha = linha.strip()  # Remove os espaços em branco
-          #              palavras.append(linha)  # Adiciona a linha na lista
-           #             
-            #        arquivo.close()  # Fecha o arquivo
-             #       
-              #      numero = random.randrange(0, len(palavras))  # Gera um número aleatório
-                #     palavra_secreta = palavras[numero].upper()  # Pega a palavra secreta
-
-    palavra_secreta = "maça".upper()  # Palavra secreta
-    
-    # Lista que adiciona um _ na lista de letras acertadas baseado no tamanho da palavra secreta
-    letras_acertadas = ["_" for letras in palavra_secreta]  
+    abertura()  # Chama a função abertura
+    palavra_secreta = palavra_aleatoria()  # Chama a função palavra_secreta
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta) # Lista que adiciona um _ na lista de letras acertadas baseado no tamanho da palavra secreta
 
     enforcou = False  # Variável que diz se o jogador foi enforcado
     acertou = False  # Variável que diz se o jogador acertou a palavra secreta
@@ -58,5 +41,76 @@ def jogar():  # Função jogo_forca
         print("Você perdeu!") 
     print("Fim do jogo")  
 
+
+
+
+
+
+
+
+
+
+
+# Apartir daqui são só as funções 
+
+
+
+
+
+
+
+
+
+
+        # Isso aqui é só pra deixar o output mais bonito
+def abertura():  # Função de imprimir a abertura
+    print("*********************************") 
+    print("***Bem vindo ao jogo de Forca!***")  # Imprime na tela
+    print("*********************************")
+
+    # Isso aqui é pra chamar a palavra secreta
+def palavra_aleatoria():  # Chama a função palavra_aleatoria
+    palavra_secreta = "paralelepipedo".upper()  # Palavra secreta
+    return palavra_secreta  # Retorna a palavra secreta
+    # Esse código todo era pra fazer parte da função de cima e chamar uma palavra aleatória de um arquivo .txt, mas não funciona
+#              arquivo = open("palavras.txt", "r")  # Abre o arquivo
+#              palavras = []  # Cria uma lista vazia
+#             
+#              for linha in arquivo:  # Para cada linha no arquivo
+#                    linha = linha.strip()  # Remove os espaços em branco
+#                    palavras.append(linha)  # Adiciona a linha na lista
+#                 
+#              arquivo.close()  # Fecha o arquivo
+#               
+#              numero = random.randrange(0, len(palavras))  # Gera um número aleatório
+#              palavra_secreta = palavras[numero].upper()  # Pega a palavra secreta
+
+def inicializa_letras_acertadas(palavra):  # Função que inicializa as letras acertadas
+    return ["_" for letra in palavra]  # Retorna uma lista com "_" para cada letra da palavra secreta
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if (__name__ == "__main__"):  # Se o arquivo for executado diretamente
     jogar()  # Executa a função de jogar
+

10bd5f6fd5c9cfa2723fb92191e0b5dde3d717ea
Josué <josuebvrios@gmail.com>
2022-10-05 10:21:37 -0300

    Implementação de atributos

--- Aprendendo(não é o projeto)/4 - Python - entendendo a Orientação a Objetos/OO/conta.py
+++ Aprendendo(não é o projeto)/4 - Python - entendendo a Orientação a Objetos/OO/conta.py
@@ -1,12 +1,20 @@
 
 
-class Conta: 
+class Conta:  
     
     def __init__(self, numero, titular, saldo, limite): #cria uma conta
         print("Construindo objeto ... {}".format(self)) #imprime a mensagem
-        self.numero  = numero  #atribui o numero a conta
-        self.titular = titular #atribui o titular a conta
-        self.saldo   = saldo   #atribui o saldo a conta
-        self.limite  = limite  #atribui o limite a conta
+        self.__numero  = numero  #atribui o numero a conta
+        self.__titular = titular #atribui o titular a conta
+        self.__saldo   = saldo   #atribui o saldo a conta
+        self.__limite  = limite  #atribui o limite a conta
+        
+    def extrato(self): #função extrato
+        print("Saldo {} do titular {}".format(self.__saldo, self.__titular)) #imprime o saldo da conta    
+        
+    def deposita(self, valor): #função deposita
+        self.__saldo += valor #adiciona o valor ao saldo
     
-    
+    def saca(self, valor): #função saca
+        self.__saldo -= valor #subtrai o valor do saldo
+     

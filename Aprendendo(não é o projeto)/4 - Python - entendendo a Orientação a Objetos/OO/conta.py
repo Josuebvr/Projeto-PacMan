84f1690292ffb5d9903f2893a657370b0836a410
Josué <josuebvrios@gmail.com>
2022-10-04 21:42:40 -0300

    Classe e objeto

--- Aprendendo(não é o projeto)/4 - Python - entendendo a Orientação a Objetos/OO/conta.py
+++ Aprendendo(não é o projeto)/4 - Python - entendendo a Orientação a Objetos/OO/conta.py
@@ -1,5 +1,12 @@
 
 
-class Conta:
+class Conta: 
     
-    def __ 
+    def __init__(self, numero, titular, saldo, limite): #cria uma conta
+        print("Construindo objeto ... {}".format(self)) #imprime a mensagem
+        self.numero  = numero  #atribui o numero a conta
+        self.titular = titular #atribui o titular a conta
+        self.saldo   = saldo   #atribui o saldo a conta
+        self.limite  = limite  #atribui o limite a conta
+    
+    

71e9c0a91e12d7f8bfc767a7cead29588a803a7e
Josué <josuebvrios@gmail.com>
2022-10-05 16:24:04 -0300

    Getters e Setters

--- Aprendendo(não é o projeto)/4 - Python - entendendo a Orientação a Objetos/OO/conta.py
+++ Aprendendo(não é o projeto)/4 - Python - entendendo a Orientação a Objetos/OO/conta.py
@@ -12,13 +12,25 @@ class Conta:
     def extrato(self): #função extrato
         print("Saldo {} do titular {}".format(self.__saldo, self.__titular)) #imprime o saldo da conta    
         
-    def deposita(self, valor): #função deposita
+    def deposita(self, valor): #função deposita o valor
         self.__saldo += valor #adiciona o valor ao saldo
     
-    def saca(self, valor): #função saca
+    def saca(self, valor): #função saca o valor
         self.__saldo -= valor #subtrai o valor do saldo
         
-    def transfere(self, valor, origem, destino): #função transfere
+    def transfere(self, valor, origem, destino): #função que transfere entre as contas
         origem.saca(valor) #saca o valor da conta origem
         destino.deposita(valor) #deposita o valor na conta destino
+        
+    def get_saldo(self): #função que pega_saldo
+        return self.__saldo #retorna o saldo
+    
+    def get_titular(self): #função que devolve para o titular
+        return self.__titular #retorna o titular
+    
+    def get_limite(self): #função que pega limite
+        return self.__limite #retorna o limite
+    
+    def set_limite(self, limite): #função que seta o limite
+        self.__limite = limite #atribui o limite a conta
      

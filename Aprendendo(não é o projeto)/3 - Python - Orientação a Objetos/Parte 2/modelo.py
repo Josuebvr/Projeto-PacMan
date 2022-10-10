78729c97cca16964b490981cc5bbcdd347437e5c
Josué <josuebvrios@gmail.com>
2022-10-10 15:24:17 -0300

    Polimorfismo

--- Aprendendo(não é o projeto)/3 - Python - Orientação a Objetos/Parte 2/modelo.py
+++ Aprendendo(não é o projeto)/3 - Python - Orientação a Objetos/Parte 2/modelo.py
@@ -1,6 +1,4 @@
 # Classe Pai
-
-
 class Programa: # Classe Pai
     def __init__(self, nome, ano): # Método construtor
         self._nome = nome.title() # Atributo nome
@@ -25,8 +23,6 @@ class Programa: # Classe Pai
 
 
 # Classe de Filmes
-
-
 class Filme(Programa): # Classe Filha
     def __init__(self, nome, ano, duracao): # Método construtor
         super().__init__(nome, ano) # Herança
@@ -35,8 +31,6 @@ class Filme(Programa): # Classe Filha
       
         
 # Classe de Séries
-
-
 class Serie(Programa): # Classe Filha
     def __init__(self, nome, ano, temporadas): # Método construtor
         super().__init__(nome, ano) # Herança
@@ -45,17 +39,19 @@ class Serie(Programa): # Classe Filha
 
 
 # Definições e prints de séries de filmes
-
-
 vingadores = Filme('vingadores - guerra infinita', 2018, 160) # Instância da classe Filme
 vingadores.dar_like() # Chama o método dar_like da instância vingadores
-print(f'Nome: {vingadores.nome}, Ano: {vingadores.ano}, Duração: {vingadores.duracao}, Likes: {vingadores.likes}') # Imprime o nome, ano, temporadas e likes da série
+print(f'{vingadores.nome} - {vingadores.duracao}: {vingadores.likes}') # Imprime o nome, duração e likes da série
 
 atlanta = Serie('atlanta', 2018, 2) # Instância da classe Série
 atlanta.dar_like() # Chama o método dar_like da instância atlanta
 atlanta.dar_like() # Chama o método dar_like da instância atlanta
-print(f'Nome: {atlanta.nome}, Ano: {atlanta.ano}, Temporadas: {atlanta.temporadas}, Likes: {atlanta.likes}') # Imprime o nome, ano, temporadas e likes da série
+print(f'{atlanta.nome} - {atlanta.temporadas}: {atlanta.likes}') # Imprime o nome, temporadas e likes da série
 
+fimes_e_series = [vingadores, atlanta] # Lista de filmes e séries
 
+for programa in fimes_e_series: # Laço de repetição
+    detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas # Condição do detalhe(duração ou temporadas)
+    print(f'{programa.nome} - {detalhes} - {programa.likes}') # Imprime o nome e likes de cada filme e série
 
 

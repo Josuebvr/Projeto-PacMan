cbf80f1fe2e9fc1c44a3ed90eeff91bc3e341690
Josué <josuebvrios@gmail.com>
2022-10-10 10:09:36 -0300

    Adicionando atributos e métodos

--- Aprendendo(não é o projeto)/3 - Python - Orientação a Objetos/Parte 2/modelo.py
+++ Aprendendo(não é o projeto)/3 - Python - Orientação a Objetos/Parte 2/modelo.py
@@ -3,16 +3,32 @@ class Filme: # Classe Filme
         self.nome = nome.title() # Atributo nome
         self.ano = ano # Atributo ano
         self.duracao = duracao # Atributo duração
+        self.likes = 0 # Atributo likes
 
+    def dar_like(self): # Método dar_like
+        self.likes += 1 # Atributo likes
 
 class Serie: # Classe Série
     def __init__(self, nome, ano, temporadas): # Método construtor
         self.nome = nome.title() # Atributo nome
         self.ano = ano # Atributo ano
         self.temporadas = temporadas # Atributo temporadas
+        self.likes = 0 # Atributo likes
+
+    def dar_like(self): # Método dar like
+        self.likes += 1 # Atributo likes
+
+
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160) # Instância da classe Filme
-print(f'Nome: {vingadores.nome}, Ano: {vingadores.ano}, Duração: {vingadores.duracao}') # Imprime o nome do filme
+
+vingadores.dar_like() # Chama o método dar_like da instância vingadores
+
+print(f'Nome: {vingadores.nome}, Ano: {vingadores.ano}, Duração: {vingadores.duracao}, Likes: {vingadores.likes}') # Imprime o nome, ano, temporadas e likes da série
 
 atlanta = Serie('atlanta', 2018, 2) # Instância da classe Série
-print(f'Nome: {atlanta.nome}, Ano: {atlanta.ano}, Temporadas: {atlanta.temporadas}') # Imprime o nome, ano e temporadas da série
+
+atlanta.dar_like() # Chama o método dar_like da instância atlanta
+
+print(f'Nome: {atlanta.nome}, Ano: {atlanta.ano}, Temporadas: {atlanta.temporadas}, Likes: {atlanta.likes}') # Imprime o nome, ano, temporadas e likes da série
+
